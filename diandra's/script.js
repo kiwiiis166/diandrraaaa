e42d68585c24a1e7204d418a17d8313012a0a4a8
kiwiiis166 <anggitaaramadhanii@gmail.com>
2025-02-21 02:48:28 +0700

    Add files via upload

---
+++ diandra's/script.js
@@ -0,0 +1,6 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
